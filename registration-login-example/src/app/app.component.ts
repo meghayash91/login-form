4ec9ee120d69ce448c2eef52e296de17634e0ca5
yashaswini <meghayash91@gmail.com>
2019-10-17 12:53:04 -0700

    login and registration example

---
+++ registration-login-example/src/app/app.component.ts
@@ -0,0 +1,25 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//     moduleId: module.id.toString(),
+//     selector: 'app',
+//     templateUrl: 'app.component.html'
+// })
+
+// export class AppComponent { }
+
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'registration-login-example';
+}
+
+
+
